a3a83dab3c0264d43411b22eb5c4139756d054ba
Satyam <nakulbegana191170@gmail.com>
2025-07-12 19:13:15 +0000

    Add minor changes and tackle existing bugs

--- digital-aquarium/script.js
+++ digital-aquarium/script.js
@@ -1,20 +1,21 @@
 const allFishTypes = ["https://png.pngtree.com/png-clipart/20250222/original/pngtree-small-fish-small-fish-cartoon-cute-cartoon-picture-image_3920714.png", "https://pngcore.com/files/preview/800x800/11695315407dxwdqrgmdoeiejmjgsfn0xjaoaqphxw0zh5tdwe9aziz6wtuv053wlnnojvuew8exsrau7ni6wiptyp9ukjym3nvoaruvkksqscr.png", "https://png.pngtree.com/png-clipart/20250417/original/pngtree-blue-little-fish-cartoon-image-png-image_20858183.png", "https://png.pngtree.com/recommend-works/png-clipart/20250624/ourmid/pngtree-little-blue-fish-baby-png-image_16574545.png"];
 
 let fishCount = 1;
-let selectedFishType = "https://png.pngtree.com/png-clipart/20250222/original/pngtree-small-fish-small-fish-cartoon-cute-cartoon-picture-image_3920714.png";
+let currentView = "2D";
+let selectedFishType = allFishTypes[0];
 
 const aquariumContainer = document.querySelector(".container");
 const fishContainer = document.getElementById("fishes");
 
 
 function makeContainer3D() {
-      aquariumContainer.classList.add("container3d");
-  
-}
-
-function makeContainer2D() {
-      aquariumContainer.classList.remove("container3d");
-  
+  if (currentView === "2D") {
+    aquariumContainer.classList.add("container3d");
+    currentView = "3D";
+  } else {
+    aquariumContainer.classList.remove("container3d");
+    currentView = "2D";
+  }
 }
 
 function addFish() {
@@ -23,24 +24,25 @@ function addFish() {
 }
 
 function removeFish() {
-  fishCount += 1;
+  fishCount = Math.max(0, fishCount - 1);
   showFishes(fishCount);
 }
 
 function toggleFishType() {
-  const fishType = allFishTypes[Math.floor(Math.random() * allFishTypes.length)];
-
-  showFishes(fishCount, fishType);
+  selectedFishType = allFishTypes[Math.floor(Math.random() * allFishTypes.length)];
+  showFishes(fishCount, selectedFishType);
 }
+
 
 function showFishes(count = fishCount, fishUrl = selectedFishType) {
   const fragment = document.createDocumentFragment();
-  for(let i=0; i<count; i++) {
+  for (let i = 0; i < count; i++) {
     const img = document.createElement("img");
     img.src = fishUrl;
+
     fragment.appendChild(img);
   }
-  
+
   fishContainer.innerHTML = "";
   fishContainer.appendChild(fragment);
 }
